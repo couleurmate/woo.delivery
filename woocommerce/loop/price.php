b5999e37302a6fd0415ffabbca5465ac9a03f15f
Alexandre Segura <mex.zktk@gmail.com>
2018-11-12 19:07:11 +0100

    Display price per kg.

--- woocommerce/loop/price.php
+++ woocommerce/loop/price.php
@@ -8,6 +8,8 @@ global $product;
 
 $origin_attribute = null;
 $brand_attribute = null;
+$unit_weight_attribute = null;
+
 foreach ($product->attributes as $attribute) {
     if ($attribute->get_name() === 'pa_origin') {
         $origin_attribute = $attribute;
@@ -15,10 +17,14 @@ foreach ($product->attributes as $attribute) {
     if ($attribute->get_name() === 'pa_brand') {
         $brand_attribute = $attribute;
     }
+    if ($attribute->get_name() === 'pa_unit_weight') {
+        $unit_weight_attribute = $attribute;
+    }
 }
 
 $origins = array();
 $brands = array();
+$weights = array();
 
 if ($origin_attribute) {
     foreach ($origin_attribute->get_terms() as $term) {
@@ -30,6 +36,23 @@ if ($brand_attribute) {
     foreach ($brand_attribute->get_terms() as $term) {
         $brands[] = $term->name;
     }
+}
+
+if ($unit_weight_attribute) {
+    foreach ($unit_weight_attribute->get_terms() as $term) {
+        $weights[] = $term->name;
+    }
+}
+
+$price_per_kg = null;
+
+if (count($weights) > 0) {
+    $weight = current($weights);
+    if (1 === preg_match('/([0-9]+)/', $weight, $matches)) {
+        $grams = $matches[1];
+        $price_per_kg = ($product->get_price() * 1000) / $grams;
+    }
+
 }
 
 ?>
@@ -49,7 +72,13 @@ if ($brand_attribute) {
         <td class="aurayonbio__after-item-table__brand">
             <?php echo implode(', ', $brands) ?>
         </td>
-        <td>&nbsp;</td>
+        <td class="aurayonbio__after-item-table__price-per-kg">
+            <?php if (null !== $price_per_kg) : ?>
+                <?php echo wc_price($price_per_kg) ?>/kg
+            <?php else : ?>
+                &nbsp;
+            <?php endif; ?>
+        </td>
     </tr>
 </table>
 
