a045741072a9910cb8b4b35a1a6be544fd0e02ed
PasLoin <raphael0arnould@gmail.com>
2020-04-17 17:09:05 +0200

    Create admin-new-order.php

---
+++ woocommerce/emails/admin-new-order.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Admin new order email
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/emails/admin-new-order.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce/Templates/Emails/HTML
+ * @version 3.7.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/*
+ * @hooked WC_Emails::email_header() Output the email header
+ */
+do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
+
+<?php /* translators: %s: Customer billing full name */ ?>
+<p><?php printf( esc_html__( 'You’ve received the following order from %s:', 'woocommerce' ), $order->get_formatted_b$
+<?php
