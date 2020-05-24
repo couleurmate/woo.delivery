c89313ee3f833f760c15a2ebfeb396b4d4db46df
PasLoin <raphael0arnould@gmail.com>
2020-05-24 14:45:56 +0200

    Create wp-config.php

---
+++ conf/wp-config.php
@@ -0,0 +1,42 @@
+<?php
+
+// database connection
+define( 'DB_NAME', 'wordpress' );
+define( 'DB_USER', 'wordpress' );
+define( 'DB_PASSWORD', 'wordpress' );
+define( 'DB_HOST', 'localhost' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
+$table_prefix = 'wp_';
+
+// generate new salts: https://api.wordpress.org/secret-key/1.1/salt
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
+
+// constants
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
+define( 'WP_DEBUG_DISPLAY', true );
+
+define( 'WP_MEMORY_LIMIT', '256M' );
+define( 'WP_MAX_MEMORY_LIMIT', '256M' );
+
+// Force SSL
+if ( isset( $_SERVER['HTTP_HOST'] ) ) {
+	$_SERVER['HTTPS'] = 'on';
+	define( 'WP_HOME', 'https://' . $_SERVER['HTTP_HOST'] );
+	define( 'WP_SITEURL', 'https://' . $_SERVER['HTTP_HOST'] );
+}
+
+// That's all, stop editing!
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+require_once( ABSPATH . 'wp-settings.php' );
