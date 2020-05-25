634fe3cf4f0225a7db339c81d04abf209d26be4d
PasLoin <raphael0arnould@gmail.com>
2020-05-25 07:47:11 +0200

    Update .bashrc.sh

--- conf/.bashrc.sh
+++ conf/.bashrc.sh
@@ -50,7 +50,10 @@ function wp-setup () {
     # some test : 
     # cp $HOME/${GITPOD_REPO_ROOT}/conf/wp-config.php ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/wp-config.php
 #cp conf/wp-config.php ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/wp-config.php
-cp $HOME/wp-config.php ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/wp-config.php
+# cp $HOME/wp-config.php ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/wp-config.php
+
+cp $HOME/woo-delivery/conf/wp-config.php ${GITPOD_REPO_ROOT}/${APACHE_DOCROOT}/wp-config.php
+
 # END create a wp-config.php
 
   # Setup WordPress database
