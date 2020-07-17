cd55c61c043de6dac26b6a4693050233b7a8344a
PasLoin <raphael0arnould@gmail.com>
2020-07-17 07:41:33 +0200

    Create holiday.php
    need to paste to function.php 
    Code just to have exemple

---
+++ holiday.php
@@ -0,0 +1,16 @@
+// Trigger Holiday Mode
+
+// add_action ('init', 'bbloomer_woocommerce_holiday_mode');
+
+
+// Disable Cart, Checkout, Add Cart
+
+// function bbloomer_woocommerce_holiday_mode() {
+   remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
+   remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+   remove_action( 'woocommerce_proceed_to_checkout', 'woocommerce_button_proceed_to_checkout', 20 );
+   remove_action( 'woocommerce_checkout_order_review', 'woocommerce_checkout_payment', 20 );
+   add_action( 'woocommerce_before_main_content', 'bbloomer_wc_shop_disabled', 5 );
+   add_action( 'woocommerce_before_cart', 'bbloomer_wc_shop_disabled', 5 );
+   add_action( 'woocommerce_before_checkout_form', 'bbloomer_wc_shop_disabled', 5 );
+}
